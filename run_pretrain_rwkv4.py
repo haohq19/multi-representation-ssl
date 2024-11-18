6d651f5619f006e240bf8b2f4455c7d91a32731b
haohq19 <haohq19@gmail.com>
2024-11-18 10:29:35 +0800

    fix bugs

--- run_pretrain_rwkv4.py
+++ run_pretrain_rwkv4.py
@@ -13,6 +13,122 @@ from models.loss.multi_representation_loss import MultiRepresentationLoss
 from engine_for_pretraining import train
 
 
+def load_config(config_path):
+    config_base_path = os.path.join(config_path, 'base.yaml')
+    with open(config_base_path, 'r') as f:
+        config = yaml.safe_load(f)
+    
+    # dynamically load config.models.model and config.models.tokenizer
+    config_models = config['models']
+    model_name = config_models['model']
+    tokenizer_name = config_models['tokenizer']
+    config_model_path = os.path.join(config_path, f'models/{model_name}.yaml')
+    config_tokenizer_path = os.path.join(config_path, f'models/{tokenizer_name}.yaml')
+    with open(config_model_path, 'r') as f:
+        config_model = yaml.safe_load(f)
+        config_models['model'] = config_model
+    with open(config_tokenizer_path, 'r') as f:
+        config_tokenizer = yaml.safe_load(f)
+        config_models['tokenizer'] = config_tokenizer
+    
+    return config
+
+
+def update_config_from_args(config):
+    parser = argparse.ArgumentParser()
+    # data
+    parser.add_argument('--dataset', type=str, help='dataset')
+    parser.add_argument('--root', type=str, help='path to dataset')
+    parser.add_argument('--count', type=int, help='number of events per sample')
+    parser.add_argument('--stride', type=int, help='stride of slicing')
+    parser.add_argument('--input_len', type=int, help='length of input sequence')
+    parser.add_argument('--rep_len', type=int, help='length of output sequence')
+    parser.add_argument('--patch_size', type=int, help='patch size')
+    parser.add_argument('--train_split', type=float, help='train split ratio')
+    parser.add_argument('--shuffle', type=bool, help='shuffle dataset')
+    parser.add_argument('--num_workers', type=int, help='number of workers')
+    
+    # models
+    parser.add_argument('--model', type=str, help='model name')
+    parser.add_argument('--tokenizer', type=str, help='tokenizer name')
+    parser.add_argument('--d_model', type=int, help='dimension of embedding')
+    parser.add_argument('--depth', type=int, help='number of encoder layers')
+    parser.add_argument('--d_ffn', type=int, help='dimension of feedforward')
+    
+    # training
+    parser.add_argument('--batch_size', type=int, help='batch size')
+    parser.add_argument('--device_id', type=int, help='GPU id to use, invalid when distributed training')
+    parser.add_argument('--nepochs', type=int, help='number of epochs')
+    parser.add_argument('--lr', type=float, help='learning rate')
+    parser.add_argument('--output_dir', type=str, help='dir to save')
+    parser.add_argument('--save_freq', type=int, help='save frequency')
+    parser.add_argument('--optimizer', type=str, help='optimizer')
+    parser.add_argument('--step_size', type=int, help='step size of scheduler')
+    parser.add_argument('--gamma', type=float, help='gamma of scheduler')
+    parser.add_argument('--resume', action='store_true', help='resume from latest checkpoint')
+    
+    # loss
+    parser.add_argument('--loss_fn', type=str, help='loss function')
+    parser.add_argument('--pred_frame', type=bool, help='predict frame')
+    parser.add_argument('--pred_ts', type=bool, help='predict timestamp')
+    parser.add_argument('--pred_next_frame', type=bool, help='predict next frame')
+
+    # dist
+    parser.add_argument('--dist-url', default='env://', help='url used to set up distributed training')
+    parser.add_argument('--backend', default='gloo', help='distributed backend')
+
+    # misc
+    parser.add_argument('--seed', type=int, help='random seed')
+
+    args = parser.parse_args()
+    
+    # data  
+    for key in ['dataset', 'root', 'count', 'stride', 'input_len', 'rep_len', 'patch_size', 'train_split', 'shuffle', 'num_workers']:
+        if getattr(args, key) is not None:
+            config['data'][key] = getattr(args, key)
+            
+    # models
+    for key in ['model', 'tokenizer', 'd_model', 'depth', 'd_ffn']:
+        if getattr(args, key) is not None:
+            config['models'][key] = getattr(args, key)
+            
+    # training
+    for key in ['batch_size', 'device_id', 'nepochs', 'lr', 'output_dir', 'save_freq', 'optimizer', 'step_size', 'gamma', 'resume']:
+        if getattr(args, key) is not None:
+            config['training'][key] = getattr(args, key)
+            
+    # loss
+    for key in ['loss_fn', 'pred_frame', 'pred_ts', 'pred_next_frame']:
+        if getattr(args, key) is not None:
+            config['loss'][key] = getattr(args, key)
+            
+    # dist
+    for key in ['dist_url', 'backend']:
+        if getattr(args, key) is not None:
+            config['dist'][key] = getattr(args, key)
+            
+    # misc
+    if args.seed is not None:
+        config['misc']['seed'] = args.seed
+        
+    # update config
+    # data
+    config['data']['batch_size'] = config['training']['batch_size']
+    
+    # models
+    patch_size = config['data']['patch_size']
+    pred_frame = config['loss']['pred_frame']
+    pred_ts = config['loss']['pred_ts']
+    pred_next_frame = config['loss']['pred_next_frame']
+    ntargets = pred_frame + pred_ts + pred_next_frame
+    config['models']['model']['num_classes'] = 2 * patch_size * patch_size * ntargets
+    config['models']['tokenizer']['patch_size'] = patch_size
+    config['models']['tokenizer']['d_embed'] = config['models']['model']['d_model']
+    
+    return config
+
+
+
 def init_seed(seed):
     random.seed(seed)
     torch.manual_seed(seed)
@@ -122,119 +238,6 @@ def main(config):
         save_freq=config_training['save_freq'],
         dist=config_dist['dist'],
     )
-    
-
-def load_config(config_path):
-    with open(config_path, 'r') as f:
-        config = yaml.safe_load(f)
-    
-    # dynamically load model and tokenizer config
-    model_name = config['models']['model']
-    tokenizer_name = config['models']['tokenizer']
-    config_model_path = os.path.join(config_path.replace('base.yaml', ''), f'models/{model_name}.yaml')
-    config_tokenizer_path = os.path.join(config_path.replace('base.yaml', ''), f'models/{tokenizer_name}.yaml')
-    with open(config_model_path, 'r') as f:
-        config_model = yaml.safe_load(f)
-        config['models']['model'] = config_model
-    with open(config_tokenizer_path, 'r') as f:
-        config_tokenizer = yaml.safe_load(f)
-        config['models']['tokenizer'] = config_tokenizer
-    
-    return config
-
-
-def update_config_from_args(config):
-    parser = argparse.ArgumentParser()
-    # data
-    parser.add_argument('--dataset', type=str, help='dataset')
-    parser.add_argument('--root', type=str, help='path to dataset')
-    parser.add_argument('--count', type=int, help='number of events per sample')
-    parser.add_argument('--stride', type=int, help='stride of slicing')
-    parser.add_argument('--input_len', type=int, help='length of input sequence')
-    parser.add_argument('--rep_len', type=int, help='length of output sequence')
-    parser.add_argument('--patch_size', type=int, help='patch size')
-    parser.add_argument('--train_split', type=float, help='train split ratio')
-    parser.add_argument('--shuffle', type=bool, help='shuffle dataset')
-    parser.add_argument('--num_workers', type=int, help='number of workers')
-    
-    # models
-    parser.add_argument('--model', type=str, help='model name')
-    parser.add_argument('--tokenizer', type=str, help='tokenizer name')
-    parser.add_argument('--d_model', type=int, help='dimension of embedding')
-    parser.add_argument('--depth', type=int, help='number of encoder layers')
-    parser.add_argument('--d_ffn', type=int, help='dimension of feedforward')
-    
-    # training
-    parser.add_argument('--batch_size', type=int, help='batch size')
-    parser.add_argument('--device_id', type=int, help='GPU id to use, invalid when distributed training')
-    parser.add_argument('--nepochs', type=int, help='number of epochs')
-    parser.add_argument('--lr', type=float, help='learning rate')
-    parser.add_argument('--output_dir', type=str, help='dir to save')
-    parser.add_argument('--save_freq', type=int, help='save frequency')
-    parser.add_argument('--optimizer', type=str, help='optimizer')
-    parser.add_argument('--step_size', type=int, help='step size of scheduler')
-    parser.add_argument('--gamma', type=float, help='gamma of scheduler')
-    parser.add_argument('--resume', action='store_true', help='resume from latest checkpoint')
-    
-    # loss
-    parser.add_argument('--loss_fn', type=str, help='loss function')
-    parser.add_argument('--pred_frame', type=bool, help='predict frame')
-    parser.add_argument('--pred_ts', type=bool, help='predict timestamp')
-    parser.add_argument('--pred_next_frame', type=bool, help='predict next frame')
-
-    # dist
-    parser.add_argument('--dist-url', default='env://', help='url used to set up distributed training')
-    parser.add_argument('--backend', default='gloo', help='distributed backend')
-
-    # misc
-    parser.add_argument('--seed', type=int, help='random seed')
-
-    args = parser.parse_args()
-    
-    # data  
-    for key in ['dataset', 'root', 'count', 'stride', 'input_len', 'rep_len', 'patch_size', 'train_split', 'shuffle', 'num_workers']:
-        if getattr(args, key) is not None:
-            config['data'][key] = getattr(args, key)
-            
-    # models
-    for key in ['model', 'tokenizer', 'd_model', 'depth', 'd_ffn']:
-        if getattr(args, key) is not None:
-            config['models'][key] = getattr(args, key)
-            
-    # training
-    for key in ['batch_size', 'device_id', 'nepochs', 'lr', 'output_dir', 'save_freq', 'optimizer', 'step_size', 'gamma', 'resume']:
-        if getattr(args, key) is not None:
-            config['training'][key] = getattr(args, key)
-            
-    # loss
-    for key in ['loss_fn', 'pred_frame', 'pred_ts', 'pred_next_frame']:
-        if getattr(args, key) is not None:
-            config['loss'][key] = getattr(args, key)
-            
-    # dist
-    for key in ['dist_url', 'backend']:
-        if getattr(args, key) is not None:
-            config['dist'][key] = getattr(args, key)
-            
-    # misc
-    if args.seed is not None:
-        config['misc']['seed'] = args.seed
-        
-    # update config
-    # data
-    config['data']['batch_size'] = config['training']['batch_size']
-    
-    # models
-    patch_size = config['data']['patch_size']
-    pred_frame = config['loss']['pred_frame']
-    pred_ts = config['loss']['pred_ts']
-    pred_next_frame = config['loss']['pred_next_frame']
-    ntargets = pred_frame + pred_ts + pred_next_frame
-    config['models']['model']['num_classes'] = 2 * patch_size * patch_size * ntargets
-    config['models']['tokenizer']['patch_size'] = patch_size
-    config['models']['tokenizer']['d_embed'] = config['models']['model']['d_model']
-    
-    return config
 
 
 if __name__ == '__main__':
